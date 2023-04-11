178cacf6aec635d63e6075aa489f5e19556e3521
Timur Kaiser <t.kayzer@tengebank.uz>
2023-04-11 16:44:04 +0500

    router

--- src/controllers/todo.controller.ts
+++ src/controllers/todo.controller.ts
@@ -1,10 +1,19 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { TodoModel } from "../model/todo.model";
 
 export class TodoController {
+
+    public router: Router;
+    constructor() {
+        this.router = Router();
+        this.router.post('/todos', this.createTodo);
+    }
+
     createTodo(req: Request, res: Response) {
         const newTodo = req.body;
         TodoModel.create(newTodo);
         res.status(201).json(newTodo);
     }
 }
+
+export const todoController = new TodoController();
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
-
+import { todoController } from './controllers/todo.controller';
 
 export const app = express();
 
 app.get("/", (req, res) => {
     res.json("Hello world");
 });
+
+app.use(todoController.router);
 
 app.listen(3000, () => { console.log("Server is running on 3000"); });
